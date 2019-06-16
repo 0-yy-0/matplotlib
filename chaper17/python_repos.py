9633b05ebdbbb9af64376afa7a886259ea87d05c
0-yy-0 <310484121@qq.com>
2019-06-16 21:41:18 +0800

    chaper 18 end

--- chaper17/python_repos.py
+++ chaper17/python_repos.py
@@ -19,9 +19,9 @@ print("Total repositories:", response_dict['total_count'])
 # 探索有关仓库的信息
 repo_dicts = response_dict['items']
 
-"""
-print("Repositories returned: ", len(repo_dicts))
 
+print("Repositories returned: ", len(repo_dicts))
+"""
 # 研究第一个仓库
 repo_dict = repo_dicts[0]
 print("\nKeys: ", len(repo_dict))
@@ -40,17 +40,23 @@ for repo_dict in repo_dicts:
     print('Description:', repo_dict['description'])
 """
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
 
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
-my_config.show_legend=False
+my_config.show_legend = False
 my_config.title_font_size = 24
 my_config.label_font_size = 14
 my_config.major_label_font_size = 18
@@ -58,10 +64,9 @@ my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
 
-chart = pygal.Bar(my_config, style = my_style)
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
-
