b96383c266757f69f0d5c76a51126e3ae49d20f3
0-yy-0 <310484121@qq.com>
2019-06-17 22:40:03 +0800

    19.1.2 错误：url not defined

--- chaper18/learning_log/learning_logs/urls.py
+++ chaper18/learning_log/learning_logs/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('topics', views.topics, name='topics'),
 
     # 特定主题的详细页面
-    path('topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+    path('topics/(? P<topic_id>\d+)/', views.topic, name='topic'),
+
+    # 用于添加新主题的网页
+    path('new_topic/', views.new_topic, name='new_topic'),
+
 ]
